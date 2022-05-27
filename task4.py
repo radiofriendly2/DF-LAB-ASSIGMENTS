aa37acfebefd470b79b7ef5da1a8ef4f2c880028
shehzil123 <102830822+shehzil123@users.noreply.github.com>
2022-05-28 02:45:05 +0500

    Add files via upload

---
+++ task4.py
@@ -0,0 +1,30 @@
+import argparse
+
+import re
+
+import os.path  
+
+def check_dir(file, regex):
+
+if(os.path.isdir(file) == True):
+
+directory = (os.listdir(file))
+for directories in directory:
+
+
+if(regex.search(file)):
+print("File Found, Named = ", file)
+file += "/" + directories
+check_dir(file, regex)
+
+
+if _name=="main_":
+    parser=argparse.ArgumentParser()
+    
+    parser.add_argument('-f', help = 'argument for file')
+    
+    parser.add_argument('-r', help = 'argument for regex')
+    args = parser.parse_args()
+    check_dir(args.f, re.compile(args.r))
+
+
